49f2070590aa90bdc618c7cf44d3875b09d18edd
Joy <github.joysaha@gmail.com>
2024-03-29 12:09:18 +0600

    create component and changes path

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2>This is root file</h2>
+            
+        </div>
+    );
+};
+
+export default Root;

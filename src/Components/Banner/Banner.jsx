7188176cef88174e8482d5cbd057cbd1c431dfd6
Joy <github.joysaha@gmail.com>
2024-04-14 11:16:02 +0600

    banner recreate and bug fixing

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,21 +3,19 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content  flex-col lg:flex-row-reverse">
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center px-1 mt-6 lg:px-20">
+        <div className="space-y-4">
+          <h2 className="text-5xl font-bold">Hi!</h2>
+          <h2 className="text-5xl font-bold">I am Joy Saha</h2>
+          <p className="text-xl font-semibold">I am a Web Developer</p>
+          <p className="text-xl font-semibold">From Bangladesh</p>
+          <button className="btn btn-primary text-white">Check Resume</button>
+        </div>
+        <div>
           <img
             src="https://i.ibb.co/HDTkQ1b/dddd.png"
-            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[400px] md:max-h-[400px] rounded-[50%] border-2 border-solid border-black"
+            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[350px] md:max-h-[350px] rounded-[50%] border-2 border-solid border-black"
           />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
         </div>
       </div>
     </div>

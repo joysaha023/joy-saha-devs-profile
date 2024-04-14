59454b2f16a03702e2b380e1f8fca0e9854c9ddd
Joy <github.joysaha@gmail.com>
2024-04-14 12:22:01 +0600

    typewritter added and modified banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import {Bio} from "../../data/constants"
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Banner = () => {
+ const [text] = useTypewriter({
+  words: ['MERN Stack Developer', 'Front-End Developer', 'Web Developer'],
+  autoStart: true,
+  loop: {},
+ });
+
+
   return (
     <div>
       <div className="flex flex-col-reverse lg:flex-row justify-between items-center px-2 mt-6">
         <div className="space-y-4">
           <h2 className="text-5xl font-bold">Hi!</h2>
           <h2 className="text-5xl font-bold">I am Joy Saha</h2>
-          <p className="text-xl font-semibold">I am a Web Developer</p>
+          <p className="text-xl font-semibold">I am a <span className="text-[#0077be]">
+            {text}
+            </span><span className="text-[#0077be]"><Cursor></Cursor></span></p>
           <p className="text-base font-medium w-full lg:w-[500px]">{Bio.description}</p>
-          <button className="btn btn-primary text-white">Check Resume</button>
+          <button className="btn bg-[#0077be] text-white">Check Resume</button>
         </div>
         <div>
           <img
             src="https://i.ibb.co/HDTkQ1b/dddd.png"
-            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[400px] md:max-h-[400px] rounded-[50%] border-2 border-solid border-black"
+            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[400px] md:max-h-[400px] rounded-[50%] border-2 border-solid border-[#0077be]"
           />
         </div>
       </div>

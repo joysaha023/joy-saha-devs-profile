a385997ece8e633f8d0097cd418563442799971a
Joy <github.joysaha@gmail.com>
2024-04-15 22:51:34 +0600

    contact section push

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIosMail } from "react-icons/io";
 
 const Contact = () => {
   return (
@@ -6,6 +7,12 @@ const Contact = () => {
       <h2 className="text-4xl my-16 font-bold text-center">
         Contact <span className="text-[#0077be]">Me</span>
       </h2>
+      <div>
+        <div className="flex items-center gap-2">
+          <IoIosMail className="text-2xl " />
+          <p>joysahacse23@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 };

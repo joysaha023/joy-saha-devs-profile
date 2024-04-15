0bd10f33aab090436955a09a5c8eb1b02dc75634
Joy <github.joysaha@gmail.com>
2024-04-15 17:09:53 +0600

    my project section some edits

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const MyWork = () => {
+  
+
   return (
     <div className="mt-6">
       <h2 className="text-4xl font-bold text-center">
-        My <span className="text-[#0077be]">Work</span>
+        My <span className="text-[#0077be]">Projects</span>
       </h2>
+     
     </div>
   );
 };

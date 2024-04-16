c9ca1eb70e324bd7fd2880f77d9dd121011e8512
Joy <github.joysaha@gmail.com>
2024-04-16 22:46:10 +0600

    banner section edits

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,12 +14,12 @@ const About = () => {
         <div className="">
           <img
             src="https://i.ibb.co/h1fVqCg/dsd.jpg"
-            className=" w-full h-full max-w-[280px] max-h-[280px] md:max-w-[300px] rounded-md md:max-h-[400px]  border-2 border-solid border-[#0077be]"
+            className=" w-full h-full max-w-[280px] max-h-[300px] md:max-w-[300px] rounded-md md:max-h-[400px]  border-2 border-solid border-[#0077be]"
           />
         </div>
         <div className="">
-          <p className=" lg:w-[700px]">
-          I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.Committed to collaborating effectively within a team and enthusiastically taking on challenging tasks to contribute to the company's success.
+          <p className=" w-full lg:w-[600px]">
+          Innovative, passionate and hard working certified  Web Developer with more than 1 years of experience  in Web Design and Web Development. Gained a lot experience in web development, specialized in frontend technologies.Committed to collaborating effectively within a team and enthusiastically taking on challenging tasks to contribute to the company's success.
           </p>
           <AboutTab></AboutTab>
         </div>
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
       <div className="flex flex-col-reverse lg:flex-row justify-between items-center px-2 mt-6">
         <div className="space-y-4">
           <h2 className="text-5xl font-bold">Hi!</h2>
-          <h2 className="text-5xl font-bold">I am Joy Saha</h2>
+          <h2 className="text-5xl font-bold">I am <span className=" text-[#0077be]">Joy Saha</span></h2>
           <p className="text-xl font-semibold">I am a <span className="text-[#0077be]">
             {text}
             </span><span className="text-[#0077be]"><Cursor></Cursor></span></p>
@@ -25,7 +25,7 @@ const Banner = () => {
         <div>
           <img
             src="https://i.ibb.co/HDTkQ1b/dddd.png"
-            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[400px] md:max-h-[400px] rounded-[50%] border-2 border-solid border-[#0077be]"
+            className="relative w-full h-full max-w-[280px] max-h-[280px] md:max-w-[400px] md:max-h-[400px] rounded-[50%] lg:mr-16 border-2 border-solid border-[#0077be]"
           />
         </div>
       </div>

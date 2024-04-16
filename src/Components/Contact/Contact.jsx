0be7b95a5c0b9ade672880be2210f1a888594e73
Joy <github.joysaha@gmail.com>
2024-04-16 09:56:45 +0600

    contact me section edits

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IoIosMail } from "react-icons/io";
+import { FaPhoneVolume, FaLocationDot } from "react-icons/fa6";
 
 const Contact = () => {
   return (
@@ -7,10 +8,41 @@ const Contact = () => {
       <h2 className="text-4xl my-16 font-bold text-center">
         Contact <span className="text-[#0077be]">Me</span>
       </h2>
-      <div>
-        <div className="flex items-center gap-2">
-          <IoIosMail className="text-2xl " />
-          <p>joysahacse23@gmail.com</p>
+      <div className="flex flex-col md:flex-row  px-10">
+        <div className="flex-1">
+          <h2 className="text-2xl">Reach Out to Me Directly</h2>
+          <div className="flex items-center gap-2">
+            <IoIosMail className="text-2xl " />
+            <p>joysahacse23@gmail.com</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <FaPhoneVolume className="text-2xl " />
+            <p>+8801861490690</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <FaLocationDot className="text-2xl " />
+            <p>Permanent Address: Brahmanbaria, Chittagong, Bangladesh</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <h2 className="text-2xl">I'd Love to Here From You</h2>
+          <form className="flex flex-col space-y-6">
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              className="input input-bordered w-full "
+            />
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              className="input input-bordered w-full "
+            />
+            <textarea
+              className="textarea textarea-bordered"
+              placeholder="Write Your Message"
+            ></textarea>
+            <input type="submit" value="submit" className="btn bg-[#0077be] w-1/4" />
+          </form>
         </div>
       </div>
     </div>

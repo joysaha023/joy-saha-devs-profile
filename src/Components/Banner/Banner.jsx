41f3def11ba94f935ce19514a7110f9894beed8d
Joy <github.joysaha@gmail.com>
2024-06-11 10:21:36 +0600

    drive link added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
             {text}
             </span><span className="text-[#0077be]"><Cursor></Cursor></span></p>
           <p className="text-base font-medium w-full lg:w-[500px]">{Bio.description}</p>
-          <a href="https://drive.google.com/file/d/1-kOJhpfL6-9_uPguXzwsR-0Gie2zu57S/view?usp=sharing" target="blank" rel="noopener noreferrer" download="" className="btn bg-[#0077be] text-white">Download Resume</a>
+          <a href="https://drive.google.com/file/d/1AlVXeC8iU5sOTxVrugFQDwM0iz8kOt1O/view?usp=sharing" target="blank" rel="noopener noreferrer" download="" className="btn bg-[#0077be] text-white">Download Resume</a>
         </div>
         <div>
           <img
